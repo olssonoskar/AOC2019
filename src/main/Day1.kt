989477f558f4038487c115a0e4bf1fbb1c230bcc
Arklights <oskarol@student.chalmers.se>
2019-12-01 14:07:27 +0100

    Completion of day1

---
+++ src/main/Day1.kt
@@ -0,0 +1,42 @@
+package main
+
+import java.io.File
+
+fun main() {
+    val input : String  = File("src\\resources\\day1.txt").readLines().reduce{a, b -> "$a,$b" }
+    val masses = input.split(',').map { it.toInt() }
+    val day1 = Day1()
+    val part1 = day1.fuelRequired(masses)
+    println("Part1 : Total fuel required: $part1")
+    val part2 = day1.fuelRequiredAll(masses)
+    println("Part2 : Total fuel required including fuel: $part2")
+}
+
+class Day1 {
+
+    fun fuelRequired(weight: List<Int>): Int {
+        return weight.map {
+            calcFuelRequired(it)
+        }.sum()
+    }
+
+    fun fuelRequiredAll(weight: List<Int>): Int {
+        return weight.map {
+                calcFuelRequired(it) }
+            .map { it + fuelForFuel(it) }
+            .sum()
+    }
+
+    private fun fuelForFuel(fuelMass: Int) : Int {
+        val extra = calcFuelRequired(fuelMass)
+        return if (extra <= 0) {
+            0
+        } else {
+            extra + fuelForFuel(extra)
+        }
+    }
+
+    private fun calcFuelRequired(mass : Int): Int {
+        return Math.floorDiv(mass, 3) - 2
+    }
+}
